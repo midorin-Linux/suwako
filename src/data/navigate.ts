e584923462f5327eb3cc9810696e37b7bf7b8505
midorin-Linux <midorin.shak12@gmail.com>
2026-03-28 18:09:18 +0900

    Change favicon

--- src/data/navigate.ts
+++ src/data/navigate.ts
@@ -1,5 +1,5 @@
 export const navItems: { title: string; url: string; column?: { title: string; url: string; }[]; }[] = [
-    { title: "守矢神社とは", url: "#", column: [{ title: "守矢神社について", url: "#" }, { title: "守矢神社の歴史と神話", url: "#" }, { title: "土着信仰について", url: "#" }] },
+    { title: "守矢神社とは", url: "/about", column: [{ title: "守矢神社について", url: "/about" }, { title: "守矢神社の歴史と神話", url: "/about" }, { title: "土着信仰について", url: "/about" }] },
     { title: "御参拝のご案内", url: "#", column: [{ title: "境内のご案内", url: "#" }, { title: "守矢神社の一年", url: "#" }, { title: "はじめての諏訪大社", url: "#" }] },
     { title: "お知らせ", url: "#" },
     { title: "神事・行事", url: "#" },
